4899f021d0348be4b468ee5d73f6576faaf3787b
Shun Fukusumi <shun.fukusumi@gmail.com>
2019-03-31 09:42:31 +0900

    LICENSE added

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ from setuptools import find_packages, setup
 with open('README.md') as f:
     readme = f.read()
 
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='mylogging',
     version='0.0.2',
     description='logging package for myself',
     long_description=readme,
+    license=license,
     author='Shun Fukusumi',
     author_email='shun.fukusumi@gmail.com',
     url='https://github.com/juv-shun/mylogging',
